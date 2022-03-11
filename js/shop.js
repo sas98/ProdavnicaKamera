924f5f01bde808882d3b4d84fd82249142ae1b82
Djordje1999 <djordjen7@gmail.com>
2022-03-11 14:11:36 +0100

    zagradice mrtve

--- js/shop.js
+++ js/shop.js
@@ -40,14 +40,33 @@ window.onload = () => {
     });
   }
 
+  function getProducts() {
+    $.ajax({
+      url: "assets/data/products.json",
+      method: "get",
+      dataType: "json",
+      success: function (data) {
+        return data;
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  }
+
   function showTypes(data) {
+    var products = getProducts();
+
     let html = "";
     data.forEach((type) => {
       html += `<a
         href="#"
         class="list-group-item list-group-item-action types"
         data-custom-value="${type.id}"
-        >${type.name} <small class="text-muted">()</small></a
+        >${type.name} <small class="text-muted">(${countType(
+        products,
+        type.id
+      )})</small></a
       >`;
     });
     $(".types:first").addClass("active"); // kako dodati prvom elementu klasu active ?
